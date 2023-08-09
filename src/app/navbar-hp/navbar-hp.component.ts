1ba395bdc6156fdbbb54bef7962a191d64749bed
Linda Loza <lindaloza8@gmail.com>
2023-08-08 22:18:52 -0600

    cree el componente navbar

---
+++ src/app/navbar-hp/navbar-hp.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'navbar-hp',
+  templateUrl: './navbar-hp.component.html',
+  styleUrls: ['./navbar-hp.component.css']
+})
+export class NavbarHpComponent {
+
+}
