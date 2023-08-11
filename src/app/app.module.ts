992521da6c7e17808a42e55856c9bcbdbd685aa3
Linda Loza <lindaloza8@gmail.com>
2023-08-10 21:14:51 -0600

    cree los componentes alumnos, casa, hechizos y carpeta img

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { NavbarHpComponent } from './navbar-hp/navbar-hp.component';
+import { AlumnosComponent } from './alumnos/alumnos.component';
+import { CasasComponent } from './casas/casas.component';
+import { HechizosComponent } from './hechizos/hechizos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarHpComponent
+    NavbarHpComponent,
+    AlumnosComponent,
+    CasasComponent,
+    HechizosComponent
   ],
   imports: [
     BrowserModule,
