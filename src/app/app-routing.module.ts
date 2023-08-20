3548496ce082d3cbaea9fff55c46b94d71ed9dfd
Linda Loza <lindaloza8@gmail.com>
2023-08-19 18:13:29 -0600

    conectamos todos los componentes del menú

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AlumnosComponent } from './alumnos/alumnos.component';
+import { CasasComponent } from './casas/casas.component';
+import { HechizosComponent } from './hechizos/hechizos.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'alumnos', component: AlumnosComponent},
+  {path: 'casas', component: CasasComponent },
+  {path: 'hechizos', component: HechizosComponent},
   { path: '',   redirectTo: '/home', pathMatch: 'full' }, // redirect to `firs
 ];
 
