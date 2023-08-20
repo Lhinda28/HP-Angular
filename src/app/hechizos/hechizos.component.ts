e21db1d135d0a741149cd965567b8acbd89f7a2c
Linda Loza <lindaloza8@gmail.com>
2023-08-19 19:17:12 -0600

    quitamos la imagen de inicio que se repetía

--- src/app/hechizos/hechizos.component.ts
+++ src/app/hechizos/hechizos.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./hechizos.component.css']
 })
 export class HechizosComponent {
-
+  Hechizos='Hechizos';
+  imgHechizos='hechizos.gif'
 }
+
